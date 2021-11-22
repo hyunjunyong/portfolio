eecc9e6e07e58933109f1b5a56ed6f76a0e8bc08
hyunjunyong <hjy4649@naver.com>
2021-11-23 00:18:06 +0900

    포트폴리오 제작중

---
+++ portfolio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <Header />
+    <Intro />
+    <v-divider></v-divider>
+    <Skill />
+  </v-app>
+</template>
+
+<script>
+import Header from "./components/header.vue";
+import Intro from "./components/intro.vue";
+import Skill from "./components/skill.vue";
+export default {
+  name: "App",
+
+  components: {
+    Header,
+    Intro,
+    Skill,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ portfolio/src/components/skill.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>My Skill</v-card-title>
+
+      HTML5,CSS
+      <v-progress-linear
+        value="80"
+        height="10px"
+        color="#ec407a"
+      ></v-progress-linear>
+      Javscript
+      <v-progress-linear
+        value="80"
+        height="10px"
+        color="#f4511e"
+      ></v-progress-linear>
+      vue
+      <v-progress-linear
+        value="50"
+        height="10px"
+        color="#512da8"
+      ></v-progress-linear>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  setup() {},
+  create() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+<style scoped>
+li {
+  list-style-type: none;
+}
+</style>
