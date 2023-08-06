65bbaee55a7ad69d15cf99f8519ad2545d235e35
현준용 <hjy4649@cocen.com>
2023-08-06 13:12:20 +0900

    add detail page base

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,30 @@
 <script lang="ts" setup>
+const router = useRouter();
+
 const tableData = [
   {
     profile: '1998.01.21',
     contact: 'Email',
     contactHref: 'hjy123432@gmail.com',
+    link: false,
   },
   {
     profile: '커넥트 웨이브 재직중(2022-02 ~)',
     contact: 'Phone',
     contactHref: '010-7714-4649',
+    link: false,
   },
   {
     profile: '제주대학교 졸업(2022.08)',
     contact: 'Github',
     contactHref: 'https://github.com/hyunjunyong',
+    link: true,
   },
   {
     profile: '1998.01.21',
     contact: 'Blog',
     contactHref: 'https://hjycoinfe.tistory.com/',
+    link: true,
   },
 ];
 
@@ -29,6 +35,7 @@ const projectList = [
     openModal: false,
     content:
       '시청자 앱은 Vue3를 웹앱으로 만들었고, 백오피스는 Nuxt2로 구성되어있습니다.',
+    href: '/project/nalbang',
   },
   {
     title: '일단떠나',
@@ -36,6 +43,7 @@ const projectList = [
     openModal: false,
     content:
       'Vue2를 사용하여 리뉴얼 제작되었으며, Jquery와 Vue가 혼용되어 있는 것을 Vue로 마이그레이션했습니다.',
+    href: '/project/justgo',
   },
   {
     title: '큐픽',
@@ -43,25 +51,18 @@ const projectList = [
     openModal: false,
     content:
       'Html/Css로 기본 퍼블리싱 작업을 진행하였습니다. Jquery로 배너기능을 추가하였습니다.',
+    href: '/project/qpick',
   },
   {
     title: '잇톡',
     imageUrl: '/ittok.png',
     openModal: false,
     content: 'Nuxt3를 사용해 백오피스를 구성하였습니다.',
+    href: '/project/ittok',
   },
 ];
 
 const openModal = ref<boolean[]>([false, false, false, false]);
-const handleClose = (done: () => void) => {
-  ElMessageBox.confirm('Are you sure to close this dialog?')
-    .then(() => {
-      done();
-    })
-    .catch(() => {
-      // catch error
-    });
-};
 
 const Skill = [
   { name: 'Nuxt3', percentage: 70, color: '#34d399' },
@@ -73,6 +74,9 @@ const Skill = [
 
 const duration = (percentage: number) => Math.floor(percentage / 5);
 
+const goDetail = (index: number) => {
+  router.push(`/project/detail/${index}`);
+};
 // const progressColor = (percentage: number) => {
 //   if (percentage >= 80) return '#a7f3d0';
 //   if (percentage >= 60) return '#7dd3fc';
@@ -85,36 +89,46 @@ const duration = (percentage: number) => Math.floor(percentage / 5);
 <template>
   <div class="common-layout p-10 h-screen">
     <el-container class="h-full">
-      <el-header class="flex justify-center border-b-4">
+      <el-header class="flex justify-center">
         <span class="text-2xl font-bold">Hyun's Portfolio</span>
         <!-- 해당부분은 차후 블로그등을 만들떄 사용할 예정 -->
         <!-- <ul>
           <li></li>
         </ul> -->
       </el-header>
-      <el-container class="mt-10">
+      <el-divider />
+      <el-container>
         <el-main>
           <ul class="flex gap-5 justify-center items-center">
-            <li class="w-40 h-40">
-              <img src="/img/myimage.jpg" alt="" class="w-full h-full" />
-            </li>
             <li>
               안녕하세요 프론트엔드 개발자 현준용입니다! 혼자하는것보단
               여러사람과 협업하여 커뮤니케이션을 하는 것을 즐기고 어려운 문제에
               직면해도 해결하기 위해 끊임없이 도전하는 것을 지향합니다.
             </li>
+            <li class="w-40 h-40">
+              <img src="/img/myimage.jpg" alt="" class="w-full h-full" />
+            </li>
           </ul>
+          <el-divider />
           <el-table :data="tableData" stripe style="width: 100%" class="m-auto">
-            <el-table-column prop="profile" label="프로필" width="300" />
-            <el-table-column prop="contact" label="contact" width="600">
+            <el-table-column prop="profile" label="프로필" :min-width="30" />
+            <el-table-column prop="contact" label="contact" :min-width="70">
               <template #default="scope">
-                <el-link :href="tableData[scope.$index].contactHref">
-                  {{ tableData[scope.$index].contactHref }} :
+                <el-link
+                  v-if="tableData[scope.$index].link"
+                  :href="tableData[scope.$index].contactHref"
+                >
+                  {{ tableData[scope.$index].contact }} :
                   {{ tableData[scope.$index].contactHref }}
                 </el-link>
+                <p v-else :href="tableData[scope.$index].contactHref">
+                  {{ tableData[scope.$index].contact }} :
+                  {{ tableData[scope.$index].contactHref }}
+                </p>
               </template>
             </el-table-column>
           </el-table>
+          <el-divider />
           <ul class="grid grid-cols-3 gap-10 mt-10">
             <li v-for="(item, index) in projectList" :key="index">
               <el-card @click="openModal[index] = true" class="cursor-pointer">
@@ -134,7 +148,6 @@ const duration = (percentage: number) => Math.floor(percentage / 5);
                   v-model="openModal[index]"
                   :title="item.title"
                   width="40%"
-                  :before-close="handleClose"
                 >
                   <el-image
                     :src="`img${item.imageUrl}`"
@@ -147,10 +160,7 @@ const duration = (percentage: number) => Math.floor(percentage / 5);
                       <el-button @click="openModal[index] = false"
                         >취소</el-button
                       >
-                      <el-button
-                        type="primary"
-                        @click="openModal[index] = false"
-                      >
+                      <el-button type="primary" @click="router.push(item.href)">
                         자세히 보기
                       </el-button>
                     </span>
@@ -159,6 +169,7 @@ const duration = (percentage: number) => Math.floor(percentage / 5);
               </ClientOnly>
             </li>
           </ul>
+          <el-divider />
           <div class="mt-10">
             <h3 class="text-center">Skill</h3>
             <ul>
---
+++ pages/project/detail/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>{{ route.params.id }}번 프로젝트입니다.</div>
+</template>
+
+<style scoped></style>
